aec29717ff9ae86380e8604ac67b04f58e3b185b
Oleg <steam1-00@list.ru>
2025-05-24 11:14:50 +0300

    program plan added
    program plan updated

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// token = flags.Get(token)
+
+	// tgClient = telegram.New(token)
+
+	// fetcher = fetcher.New(tgClient)
+
+	// processor = processor.New(tgClient)
+
+	// consumer.Start(fetcher, processor)
+}
