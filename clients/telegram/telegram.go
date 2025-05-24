09c41be8dbdd80ba345ec024214581c80dd6fb75
Oleg <steam1-00@list.ru>
2025-05-24 12:25:44 +0300

    method for create client and base path added

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -7,3 +7,15 @@ type Client struct {
 	basePath string
 	client   http.Client
 }
+
+func New(host string, token string) Client {
+	return Client{
+		host:     host,
+		basePath: newBasePath(token),
+		client:   http.Client{},
+	}
+}
+
+func newBasePath(token string) string {
+	return "bot" + token
+}
