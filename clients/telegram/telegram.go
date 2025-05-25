16f167b50d93f2da6903e2ec3af36b0a92e195f2
Oleg <steam1-00@list.ru>
2025-05-25 13:05:43 +0300

    refactor: improve error handling using defer wrapper

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -1,12 +1,12 @@
 package telegram
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 	"path"
 	"strconv"
+	"telegram_bot/lib/e"
 )
 
 type Client struct {
@@ -35,7 +35,9 @@ func (c *Client) Update(offset int, limit int) ([]Update, error) {
 	// do request
 }
 
-func (c *Client) doRequest(method string, query url.Values) ([]byte, error) {
+func (c *Client) doRequest(method string, query url.Values) (data []byte, err error) {
+	defer func() { err = e.WrapIfErr("can't do request", err) }()
+
 	u := url.URL{
 		Scheme: "https",
 		Host:   c.host,
@@ -44,27 +46,27 @@ func (c *Client) doRequest(method string, query url.Values) ([]byte, error) {
 
 	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
 	if err != nil {
-		return nil, fmt.Errorf("can't do request : %w", err)
+		return nil, err
 	}
 
 	req.URL.RawQuery = query.Encode()
 	resp, err := c.client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("can't do request : %w", err)
+		return nil, err
 	}
 
 	req.URL.RawQuery = query.Encode()
 
 	resp, err = c.client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("can't do request : %w", err)
+		return nil, err
 	}
 
 	defer func() { _ = resp.Body.Close() }()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("can't read response body : %w", err)
+		return nil, err
 	}
 
 	return body, nil
