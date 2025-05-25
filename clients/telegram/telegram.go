729f1059e33972883a93f401cb8b745f158b42d5
Oleg <steam1-00@list.ru>
2025-05-25 14:06:06 +0300

    method for send message added

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -17,7 +17,8 @@ type Client struct {
 }
 
 const (
-	getUpdatesMethod = "getUpdates"
+	getUpdatesMethod  = "getUpdates"
+	sendMessageMethod = "sendMessages"
 )
 
 func New(host string, token string) Client {
@@ -51,6 +52,19 @@ func (c *Client) Update(offset int, limit int) (updates []Update, err error) {
 	}
 
 	return res.Result, nil
+}
+
+func (c *Client) SendMessage(chatId int, text string) error {
+	q := url.Values{}
+	q.Add("chat_id", strconv.Itoa(chatId))
+	q.Add("text", text)
+
+	_, err := c.doRequest(sendMessageMethod, q)
+	if err != nil {
+		return e.Wrap("can't send message", err)
+	}
+
+	return nil
 }
 
 func (c *Client) doRequest(method string, query url.Values) (data []byte, err error) {
