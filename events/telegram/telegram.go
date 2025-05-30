187cf8b935165f07255908371e574eb9871787fe
Oleg <steam1-00@list.ru>
2025-05-30 21:22:41 +0300

    helpers methods fetchText and fetchType to process Telegram updates added

---
+++ events/telegram/telegram.go
@@ -0,0 +1,36 @@
+package telegram
+
+import (
+	"telegram_bot/clients/telegram"
+	"telegram_bot/events"
+	"telegram_bot/storage"
+)
+
+type Processor struct {
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
+}
+
+func New(client *telegram.Client, storage storage.Storage) *Processor {
+	return &Processor{
+		tg:      client,
+		storage: storage,
+	}
+}
+
+func fetchText(upd telegram.Update) string {
+	if upd.Message == nil {
+		return ""
+	}
+
+	return upd.Message.Text
+}
+
+func fetchType(upd telegram.Update) events.Type {
+	if upd.Message == nil {
+		return events.Unknown
+	}
+
+	return events.Message
+}
