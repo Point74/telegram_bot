76eda64429cb133984d7a2fdbbd87d4e88647a04
Oleg <steam1-00@list.ru>
2025-06-08 13:13:39 +0300

    method for saving a page with duplicate verification and messages to yhe user added

--- events/telegram/commands.go
+++ events/telegram/commands.go
@@ -1,9 +1,13 @@
 package telegram
 
 import (
+	"context"
 	"log"
 	"net/url"
 	"strings"
+	"telegram_bot/clients/telegram"
+	"telegram_bot/lib/e"
+	"telegram_bot/storage"
 )
 
 const (
@@ -12,13 +16,13 @@ const (
 	StartCmd = "/start"
 )
 
-func (p *Processor) doCmd(text string, chatID int, username string) error {
+func (p *Processor) doCmd(ctx context.Context, text string, chatID int, username string) error {
 	text = strings.TrimSpace(text)
 
 	log.Printf("got new command '%s' from '%s'", text, username)
 
 	if isAddCmd(text) {
-		// TODO: AddPage()
+		return p.savePage(ctx, chatID, text, username)
 	}
 
 	switch text {
@@ -27,6 +31,42 @@ func (p *Processor) doCmd(text string, chatID int, username string) error {
 	case StartCmd:
 	default:
 
+	}
+}
+
+func (p *Processor) savePage(ctx context.Context, chatID int, pageURL string, username string) (err error) {
+	defer func() { err = e.WrapIfErr("can't do command: save page", err) }()
+
+	sendMsg := NewMessageSender(chatID, p.tg)
+
+	page := &storage.Page{
+		URL:      pageURL,
+		UserName: username,
+	}
+
+	isExists, err := p.storage.IsExist(ctx, page)
+	if err != nil {
+		return err
+	}
+
+	if isExists {
+		return sendMsg(msgAlreadyExist)
+	}
+
+	if err := p.storage.Save(ctx, page); err != nil {
+		return err
+	}
+
+	if err := sendMsg(msgSaved); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewMessageSender(chatID int, tg *telegram.Client) func(string) error {
+	return func(msg string) error {
+		return tg.SendMessage(chatID, msg)
 	}
 }
 
