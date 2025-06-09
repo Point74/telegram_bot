724d17663563f4674a9c4e9243cdac1cd0007cc2
Oleg <steam1-00@list.ru>
2025-06-09 15:03:00 +0300

    Consumer struct and constructor function added

---
+++ consumer/event-consumer/event-consumer.go
@@ -0,0 +1,19 @@
+package event_consumer
+
+import (
+	"telegram_bot/events"
+)
+
+type Consumer struct {
+	fetcher   events.Fetcher
+	processor events.Processor
+	batchSize int
+}
+
+func New(fetcher events.Fetcher, processor events.Processor, batchSize int) Consumer {
+	return Consumer{
+		fetcher:   fetcher,
+		processor: processor,
+		batchSize: batchSize,
+	}
+}
